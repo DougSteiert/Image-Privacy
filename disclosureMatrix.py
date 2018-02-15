b30cda878b37f4e55f8d321afd8eedf532209b62
Steiert <djsg38@mst.edu>
2018-02-15 14:54:00 -0600

    More tweaking to make this work. Had to add lists of dictionaries to each person...

--- disclosureMatrix.py
+++ disclosureMatrix.py
@@ -16,34 +16,50 @@ with open("TestNetwork.txt") as f:
         newValue = '{' + value
         newValue = newValue.rstrip()
         newDict = ast.literal_eval(newValue)
-        socialNetwork[key] = newDict
+        if key in socialNetwork:
+            if key in newDict:
+                del newDict[key]
+            for newKey, (origin, sender, sentShares, origShares) in newDict.iteritems():
+                newerDict = {}
+                newerDict[newKey] = (origin, sender, sentShares, origShares)
+                socialNetwork[key].append(newDict)
+        else:
+            socialNetwork[key] = []
+            for newKey, (origin, sender, sentShares, origShares) in newDict.iteritems():
+                newerDict = {}
+                newerDict[newKey] = (origin, sender, sentShares, origShares)
+                socialNetwork[key].append(newerDict)
 
 shareNetwork = {}
 
-# Create a share network, similar to the social network, but for easier access to get shares to people
-for person, connection in socialNetwork.iteritems():
-    if person not in allPeople:
-        allPeople.append(person)
-    directConnections = {}
-    for friend, (origin, direct, sentShares, origShares) in connection.iteritems():
-        if friend == person:
-            firstOrderShares[person] = origShares
-        if friend not in allPeople:
-            allPeople.append(friend)
-        directConnections[friend] = sentShares
-    shareNetwork[person] = directConnections
-
-
-
-# The total amount of people within the network, used later for looping
-totalPeople = len(allPeople)
+# # Create a share network, similar to the social network, but for easier access to get shares to people
+# for person, theList in socialNetwork.iteritems():
+#     if person not in allPeople:
+#         allPeople.append(person)
+#     directConnections = {}
+#     for theGoods in theList:
+#         for friend, (origin, direct, sentShares, origShares) in theGoods.items():
+#             if friend == person:
+#                 firstOrderShares[person] = origShares
+#             if friend not in allPeople:
+#                 allPeople.append(friend)
+#             directConnections[friend] = sentShares
+#     shareNetwork[person] = directConnections
 
 dgraph = nx.DiGraph()
 
 # Create a directed graph between all people in the social network
-for person, info in socialNetwork.iteritems():
-    for friend, (relation, shares) in info.iteritems():
-        dgraph.add_edge(person, friend)
+for person, theList in socialNetwork.iteritems():
+    if person not in allPeople:
+        allPeople.append(person)
+    for theGoods in theList:
+        for friend in theGoods:
+            if friend not in allPeople:
+                allPeople.append(friend)
+            dgraph.add_edge(person, friend)
+
+# The total amount of people within the network, used later for looping
+totalPeople = len(allPeople)
 
 # nx.write_graphml(dgraph, "twitter.graphml")
 
@@ -52,16 +68,14 @@ def computeScore(somePerson, aFriend, parentsList):
     sharesToPerson = 0
     sharesToFriend = 0
 
-    for acq, info in shareNetwork.iteritems():
-        if acq in parentsList:
-            for person, shares in info.iteritems():
-                if person == somePerson:
-                    sharesToPerson += shares
+    for theLists in socialNetwork[person]:
+        for friend, (originalSender, directSender, sentShares, origShares) in theLists.items():
+            if friend == aFriend:
+                sharesToFriend = sentShares
+                sharesToPerson = origShares
 
-    if aFriend in shareNetwork[somePerson]:
-        sharesToFriend = shareNetwork[somePerson][aFriend]
 
-    if sharesToPerson == 0 and somePerson in firstOrderShares:
+    if somePerson in firstOrderShares and origin == somePerson:
         probability = float(sharesToFriend)/(float(firstOrderShares[somePerson]))
     else:
         probability = float(sharesToFriend)/float(sharesToPerson)
@@ -82,9 +96,8 @@ def checkParents(mainFriend, parentsMap):
     return allComputed
 
 
-def computeChild(mainPerson, newFriend, probMatrix, parentsList, friendsList):
-    for aParent in dgraph.predecessors(newFriend):
-        if aParent in parentsList:
+def computeChild(mainPerson, newFriend, probMatrix, parentsList):
+    for aParent in parentsList[newFriend]:
             score = computeScore(aParent, newFriend, parentsList)
 
             if newFriend in probMatrix[mainPerson]:
@@ -136,53 +149,56 @@ def calculateProb(someone, firstFriends, probMatrix, parents):
     while firstFriends:
         for friend in firstFriends:
             friendsParents[friend] = dgraph.predecessors(friend)
-            numberParentsCalc = checkParentsTwo(probMatrix, parents, friend, someone)
+            numberParentsCalc = checkParentsTwo(probMatrix, parents, someone)
 
             if numberParentsCalc == len(friendsParents):
-                computeChild(someone, friend, probMatrix, friendsParents, firstFriends)
+                computeChild(someone, friend, probMatrix, friendsParents)
                 firstFriends.remove(friend)
             expansionRoutine(friend, firstFriends, friendsParents)
 
 
-            if friend in parents:
-                if parents[friend] is True:
-                    firstFriends.remove(friend)
-                    break
-            if friend in socialNetwork:
-                for otherFriend, junk in socialNetwork[friend].iteritems():
-                    if otherFriend in parents and parents[otherFriend] is True:
-                        pass
-                    elif otherFriend in firstFriends and otherFriend in dgraph.predecessors(friend):
-                        computeChild(someone, otherFriend, probMatrix, parents, firstFriends)
-                    elif otherFriend not in firstFriends:
-                        firstFriends.append(otherFriend)
-                        if dgraph.successors(otherFriend):
-                            parents[otherFriend] = False
-                            for eachSuccessor in dgraph.successors(otherFriend):
-                                if eachSuccessor not in firstFriends:
-                                    firstFriends.append(eachSuccessor)
-                                elif eachSuccessor in firstFriends:
-                                    computeChild(someone, eachSuccessor, probMatrix, parents, firstFriends)
-                                
-            isReady = checkParents(friend, parents)
-            if isReady:
-                computeChild(someone, friend, probMatrix, parents, firstFriends)
-                firstFriends.remove(friend)
+            # if friend in parents:
+            #     if parents[friend] is True:
+            #         firstFriends.remove(friend)
+            #         break
+            # if friend in socialNetwork:
+            #     for otherFriend, junk in socialNetwork[friend].iteritems():
+            #         if otherFriend in parents and parents[otherFriend] is True:
+            #             pass
+            #         elif otherFriend in firstFriends and otherFriend in dgraph.predecessors(friend):
+            #             computeChild(someone, otherFriend, probMatrix, parents, firstFriends)
+            #         elif otherFriend not in firstFriends:
+            #             firstFriends.append(otherFriend)
+            #             if dgraph.successors(otherFriend):
+            #                 parents[otherFriend] = False
+            #                 for eachSuccessor in dgraph.successors(otherFriend):
+            #                     if eachSuccessor not in firstFriends:
+            #                         firstFriends.append(eachSuccessor)
+            #                     elif eachSuccessor in firstFriends:
+            #                         computeChild(someone, eachSuccessor, probMatrix, parents, firstFriends)
+            #
+            # isReady = checkParents(friend, parents)
+            # if isReady:
+            #     computeChild(someone, friend, probMatrix, parents, firstFriends)
+            #     firstFriends.remove(friend)
 
 loop = 1
 finalProbMatrix = {}
 for person in allPeople:
-    # print loop, person
-    # loop += 1
+    print loop, person
+    loop += 1
     priorityQueue = []
     finalProbMatrix[person] = {}
     parents = {}
     if person in socialNetwork:
-        for friend, info in socialNetwork[person].iteritems():
-            priorityQueue.append(friend)
-            # if dgraph.successors(friend):
-            #     parents[friend] = False
+        for listsOfInfo in socialNetwork[person]:
+            for theFriend, (origin, direct, sent, orig) in listsOfInfo.items():
+                if theFriend != person:
+                    priorityQueue.append(friend)
+                # if dgraph.successors(friend):
+                #     parents[friend] = False
 
+        finalProbMatrix[person][person] = 1
         calculateProb(person, priorityQueue, finalProbMatrix, parents)
     # else:
     #     finalProbMatrix[person] = {}
